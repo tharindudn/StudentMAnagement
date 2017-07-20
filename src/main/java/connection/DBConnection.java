f28c0e33eb53c7b0b8114b61dff3270c7bd4064e
tharindun <tharindun@hsenidmobile.com>
2017-07-20 11:01:27 +0530

    bug fixes in DBConnection

--- src/main/java/connection/DBConnection.java
+++ src/main/java/connection/DBConnection.java
@@ -4,30 +4,32 @@
  */
 
 package connection;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-    private static final String URL="jdbc:mysql://localhost/StudentManagement";
-    private static final String USER="root";
-    private static final String PASSWORD="921981970_v";
-    private static DBConnection dbconnection=null;
-    private static Connection con=null;
-    private DBConnection() throws ClassNotFoundException, SQLException{
+    private static final String URL = "jdbc:mysql://localhost/StudentManagement";
+    private static final String USER = "root";
+    private static final String PASSWORD = "921981970_v";
+    private static DBConnection dbconnection = null;
+    private static Connection con = null;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        con=DriverManager.getConnection(URL, USER, PASSWORD);
+        con = DriverManager.getConnection(URL, USER, PASSWORD);
     }
-    private static  DBConnection getDBConnection() throws ClassNotFoundException, SQLException{
-        if(dbconnection==null){
-            dbconnection=new DBConnection();
+    private static DBConnection getDBConnection() throws ClassNotFoundException, SQLException {
+        if (dbconnection == null) {
+            dbconnection = new DBConnection();
         }
         return dbconnection;
     }
-    public static Connection getConnectionToDB() throws ClassNotFoundException, SQLException{
+    public static Connection getConnectionToDB() throws ClassNotFoundException, SQLException {
         return getDBConnection().con;
     }
-
 }
 
 
